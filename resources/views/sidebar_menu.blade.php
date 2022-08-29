6b533b3849807bf347294b7bbfb0c442436c0386
ZSumiyabazar0303 <zorigtsumyabazar@gmail.com>
2022-08-29 14:18:16 +0800

    sss

---
+++ resources/views/sidebar_menu.blade.php
@@ -0,0 +1,25 @@
+<div id="kt_aside" class="aside aside-light aside-hoverable" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_mobile_toggle" style="background-color: #187387;">
+	<div class="aside-logo flex-column-auto" id="kt_aside_logo" style="background-color: #ffffff;">
+		<!--begin::Logo-->
+		<a href="/">
+			<img src="/assets/media/logos/ghg.jpg" alt="logo" style="height: 40px;width: 40px; margin-right: 10px;">
+		</a>
+		<!--end::Logo-->
+		<div class="flex-grow-1 ">
+			<a href="/" class=" bolder fs-5" style="text-align: center; color: #187387;">Барилгын хүлэмжийн хийн тооллогын систем</a>	
+		</div>
+	</div>
+	
+	<!--begin::Aside menu-->
+	<div class="aside-menu flex-column-fluid">
+		<!--begin::Aside Menu-->
+		<div class="hover-scroll-overlay-y my-5 my-lg-5" id="kt_aside_menu_wrapper" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer" data-kt-scroll-wrappers="#kt_aside_menu" data-kt-scroll-offset="0">
+			<!--begin::Menu-->
+			<div class="menu menu-column menu-title-gray-100 menu-state-title-secondary menu-state-icon-secondary menu-state-bullet-secondary menu-arrow-gray-100" id="#kt_aside_menu" data-kt-menu="true">
+
+			</div>
+			<!--end::Menu-->
+		</div>
+		<!--end::Aside Menu-->
+	</div>
+</div>
